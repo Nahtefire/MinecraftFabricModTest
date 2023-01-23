e2124da3c4dd1cc7164fb7966fad2bd7efe0cf50
Nahtefire <95053144+Nahtefire@users.noreply.github.com>
2023-01-23 23:16:12 +0000

    Minecraft Launches LOL

---
+++ src/main/java/com/nahtefire/testmod/TestModMain.java
@@ -0,0 +1,15 @@
+package com.nahtefire.testmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestModMain implements ModInitializer{
+    public static final String MOD_ID = "testmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
